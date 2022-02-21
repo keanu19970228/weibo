e87b9050b3eb7764b163203c115e5b686efa763c
LoCarlu <929130271@qq.com>
2022-02-21 16:19:44 +0000

    静态文件浏览器缓存问题

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,7 +2,7 @@
 @section('title','主页')
 
 @section('content')
-    <div class="jumbotron">
+    <div class="p-5 mb-4 bg-light rounded-3">
         <h1>Hello Laravel</h1>
         <p class="lead">
             你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
