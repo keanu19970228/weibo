0698364cfad2dd7815dc086544dc528c19aee6c1
LoCarlu <929130271@qq.com>
2022-02-23 17:03:44 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,6 +35,9 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        //用户注册成功后自动登录。
+        Auth::login($user);
+        
         session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show',[$user->id]);
         return;
